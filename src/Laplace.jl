0507aecedf8cab722516d643f1dd263cb1342f3f
Michael Helton <heltonmc@protonmail.com>
2021-03-29 12:01:29 -0400

    export functions

--- src/Laplace.jl
+++ src/Laplace.jl
@@ -1,5 +1,12 @@
 module Laplace
 
-# Write your package code here.
+export postwid, _PWcoeffs
+export LT_hyperbola
+export LT_hyper_fixed
+#export talbot_improved
+
+
+include("postwidder.jl")
+include("contours.jl")
 
 end
